1915ebbb974df562e1facd096b8446ef64144037
DridriLaBastos <adrien.cournand@gmail.com>
2019-04-22 13:28:41 +0200

    update

---
+++ arch/i386/assembly.s
@@ -0,0 +1,39 @@
+;This file regroups all assembly functions needed ofr the i386 platforms
+global get_cpuid, get_gdtr, get_idtr, switch_gdt, switch_idt
+
+SECTION .text
+;ecx : cpuid function to call
+;edx : addres of the CPUIDResult structur to fill
+get_cpuid:
+	mov eax, ecx
+	cpuid
+	mov dword [edx + 4 * 0], eax
+	mov dword [edx + 4 * 1], ebx
+	mov dword [edx + 4 * 2], ecx
+	mov dword [edx + 4 * 3], edx
+	ret
+
+;get_gdtr return the value stored inside the gdtr at the address stored in ecx
+get_gdtr:
+    sgdt [ecx]
+    ret
+
+;get_idtr return the value stored inside the idtr at the address stored in ecx
+get_idtr:
+    sidt [ecx]
+    ret
+
+;Switch to a new GDT defined by the datastructure to put on GDTR pointed to by ecx
+switch_gdt:
+    lgdt [ecx]
+    jmp 0x08:.next;<-- 0x08 will be loaded with a flat code segment with a privilege level to 0
+    .next:
+    ret
+
+;switch idt : take the address of an idtr structure in ecx
+; and then switches the current idt to the one described by this idtr
+; this function will be called once at the start of the system
+switch_idt:
+    lidt [ecx]
+    sti
+    ret
--- arch/i386/get_gdtr.s
+++
@@ -1,6 +0,0 @@
-global get_gdtr
-SECTION .text
-;get_gdtr return the value stored inside the gdtr at the address stored in ecx
-get_gdtr:
-    sgdt [ecx]
-    ret
