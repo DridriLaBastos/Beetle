08d20a43d454f720fc380725f974e71bba091dc4
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2022-06-25 23:10:51 +0200

    better use of inline asm template

--- beetle/arch/i386/include/port.hpp
+++ beetle/arch/i386/include/port.hpp
@@ -6,33 +6,30 @@
 namespace ARCH::I386
 {
 	static inline void outb (const uint8_t value, const uint16_t addr)
-	{ asm ("outb %%al, %1":: "a" (value), "dx" (addr)); }
+	{ asm ("outb %0, %1":: "a" (value), "dx" (addr)); }
 	static inline void outw (const uint16_t value, const uint16_t addr)
-	{ asm ("outw %%ax, %1":: "a" (value), "dx" (addr)); }
+	{ asm ("outw %0, %1":: "a" (value), "dx" (addr)); }
 	static inline void outd (const uint32_t value, const uint16_t addr)
-	{ asm ("outd %%eax, %1":: "a" (value), "dx" (addr)); }
+	{ asm ("outd %0, %1":: "a" (value), "dx" (addr)); }
 
 	static inline uint8_t inb (const uint16_t addr)
 	{
 		uint8_t result = 0;
-		asm ("xor %eax, %eax");
-		asm ("inb %1, %%al" : "=a" (result) : "dx" (addr));
+		asm ("inb %1, %0" : "=a" (result) : "dx" (addr));
 		return result;
 	}
 
 	static inline uint16_t inw (const uint16_t addr)
 	{
 		uint16_t result = 0;
-		asm("xor %eax, %eax");
-		asm ("inw %1, %%ax" : "=a" (result) : "dx" (addr));
+		asm ("inw %1, %0" : "=a" (result) : "dx" (addr));
 		return result;
 	}
 
 	static inline uint32_t ind (const uint16_t addr)
 	{
 		uint32_t result = 0;
-		asm("xor %eax, %eax");
-		asm ("ind %1, %%eax" : "=a" (result) : "dx" (addr));
+		asm ("ind %1, %0" : "=a" (result) : "dx" (addr));
 		return result;
 	}
 }
