d5ec859381080e963540ac927147043f1fd0f9b7
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2022-10-18 23:48:50 +0200

    cmake update

--- toolchain.cmake
+++ toolchain.cmake
@@ -2,11 +2,19 @@ set(CMAKE_SYSTEM_NAME Beetle)
 set(CMAKE_SYSTEM_PROCESSOR i386)
 set(CMAKE_SYSTEM_VERSION 0.0.1)
 
+cmake_minimum_required(VERSION 3.12 FATAL_ERROR) #3.12 needed to use file GLOB CONFIGURE_DEPENDS option
+
 set(TARGET_GNU_PREFIX "i686-elf-")
-set(CMAKE_C_COMPILER ${TARGET_GNU_PREFIX}gcc -ffreestanding -march=i386 -nostdlib)
-set(CMAKE_CXX_COMPILER ${TARGET_GNU_PREFIX}g++ -ffreestanding -march=i386 -nostdlib -nostdinc++)
+set(CMAKE_C_COMPILER ${TARGET_GNU_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${TARGET_GNU_PREFIX}g++)
 set(CMAKE_AR ${TARGET_GNU_PREFIX}ar)
-set(CMAKE_LINKER "${TARGET_GNU_PREFIX}ld -nostdlib")
+set(CMAKE_LINKER "${TARGET_GNU_PREFIX}ld")
+
+# The development compiler installed is not abble to compile a simple program without additional flags (libc start and
+# end routines are not provided at this stage). We tell cmake that the compilers are working, when needed, they will be
+# tested with the right flags
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
 
 find_program(XORRISO xorriso) # Needed by grub-mkrescue
 find_program(GRUB_MKRESCUE grub-mkrescue REQUIRED)
