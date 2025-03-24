8d4863a7c428ad3acbd2580acff94b240d0b76d1
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2025-03-24 22:06:47 +0100

    [USERLAND][INIT] Placeholder userland init program

---
+++ userland/init/main.c
@@ -0,0 +1,9 @@
+
+/**
+ * PID 0 This is the first user process created and it is the parent of all other processes in the system
+ */
+int main(int argc, char const *argv[])
+{
+	for(;;);
+	return 0;
+}
