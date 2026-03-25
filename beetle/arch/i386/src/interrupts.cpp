fd16f928ac524fe887c2da633459d3f50293cdae
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2026-03-25 19:26:17 +0100

    [I386] Adding error code parameter where needed

--- beetle/arch/i386/src/interrupts.cpp
+++ beetle/arch/i386/src/interrupts.cpp
@@ -10,37 +10,59 @@
 
 static uint64_t CMOSRealTimeClockCount = 0;
 
-void ARCH::I386::interruptDE  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptDB  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptNMI (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptBP  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptOF  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptBR  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptUD  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptNM  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptDF  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptCS  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptTS  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptNP  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptSS  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptGP  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptPF  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptMF  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptAC  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptMC  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptXM  (const void* ptr) { asm("xchg %bx, %bx"); }
-void ARCH::I386::interruptVE  (const void* ptr) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptDE  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptDB  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptNMI (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptBP  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptOF  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptBR  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptUD  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptNM  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptDF  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptCS  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptTS  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptNP  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptSS  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptGP  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptPF  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptMF  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptAC  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptMC  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptXM  (const void*, const int) { asm("xchg %bx, %bx"); }
+void ARCH::I386::interruptVE  (const void*, const int) { asm("xchg %bx, %bx"); }
 
-void ARCH::I386::irq0 (const void* ptr) { BEETLE::API::Schedule(); outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq1 (const void* ptr) { outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq2 (const void* ptr) { outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq3 (const void* ptr) { outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq4 (const void* ptr) { outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq5 (const void* ptr) { outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq6 (const void* ptr) { outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq7 (const void* ptr) { outb(PIC_EOI,MASTER_PIC_PORT); }
+/**
+ * Stack when calling this function:
+ * SS
+ * ESP
+ * EFLAGS
+ * CS
+ * EIP
+ */
+void ARCH::I386::irq0 (const void*)
+{
+    // __asm__ volatile(
+    //     "pushd eax\n"
+    //     "pushd ebx\n"
+    //     "pushd ecx\n"
+    //     "pushd edx\n"
+    //     "pushd edi\n"
+    //     "pushd esi\n"
+    //     "pushd ebp\n"
+    //     "movzd eax"
+    // );
+    outb(PIC_EOI,MASTER_PIC_PORT);
+}
 
-void ARCH::I386::irq8 (const void* ptr) { CMOSRealTimeClockCount += 1; outb(PIC_EOI, SLAVE_PIC_PORT); outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq9 (const void* ptr) { outb(PIC_EOI, SLAVE_PIC_PORT); outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq13 (const void* ptr) { outb(PIC_EOI, SLAVE_PIC_PORT); outb(PIC_EOI,MASTER_PIC_PORT); }
-void ARCH::I386::irq14 (const void* ptr) { outb(PIC_EOI, SLAVE_PIC_PORT); outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq1 (const void*) { outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq2 (const void*) { outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq3 (const void*) { outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq4 (const void*) { outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq5 (const void*) { outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq6 (const void*) { outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq7 (const void*) { outb(PIC_EOI,MASTER_PIC_PORT); }
+
+void ARCH::I386::irq8 (const void*) { CMOSRealTimeClockCount += 1; outb(PIC_EOI, SLAVE_PIC_PORT); outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq9 (const void*) { outb(PIC_EOI, SLAVE_PIC_PORT); outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq13 (const void*) { outb(PIC_EOI, SLAVE_PIC_PORT); outb(PIC_EOI,MASTER_PIC_PORT); }
+void ARCH::I386::irq14 (const void*) { outb(PIC_EOI, SLAVE_PIC_PORT); outb(PIC_EOI,MASTER_PIC_PORT); }
